259f4def2e4f9ab1cc561fe366ccd805882613a2
ChethanKumarK1 <chethanbittu5555@gmail.com>
2021-10-04 08:21:52 +0530

    first commit

---
+++ project/src/project/Switch.java
@@ -0,0 +1,25 @@
+package firstclasscode;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		int name=1;
+		switch (name){
+			
+			case 1:
+				System.out.println("chethan like mango");
+			break;
+			case 2:
+				System.out.println("ram like apple");
+			break;
+			case 3:
+				System.out.println("sham like grapes");
+			break;
+			case 4:
+				System.out.println("anu like wine");
+			default:System.out.println("invalid");
+		}
+
+	}
+
+}
---
+++ project/src/project/prog.java
@@ -0,0 +1,19 @@
+package firstclasscode;
+
+public class prog {
+
+	public static void main(String[] args) {
+	
+		int i,j;
+		for( i=1;i<=5;i++) {
+			System.out.print(i);
+			for(j=1;j<i;j++) {
+				System.out.print(i);
+				
+			}
+			System.out.println();
+		}
+
+	}
+
+}
